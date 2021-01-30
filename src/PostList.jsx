fa2d910311befe642d01ae8d2b509e9436bd55e0
Parker Bath <45578338+parkerbath@users.noreply.github.com>
2021-01-29 19:44:11 -0800

    Update PostList.jsx

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import {Card, Button, CardColumns} from 'react-bootstrap'
+import {Card, Button, CardColumns } from 'react-bootstrap'
 
 export default function PostList(props) {
-    const {posts} = props
+    const { posts, deletePost, displayPostInModal } = props
 
 
     return (
-    <div style={{paddingTop : 20}}>
+    <div style={{paddingTop: 20}}>
 <CardColumns>
-    {posts && 
-      posts.map((post) => (
-        <Card key ={post.id} > 
-            <Card.Body >
+    {posts && posts.map((post) => (
+        <Card key={post.id}> 
+            <Card.Body onClick= {() => displayPostInModal(post.id)}>
                 <Card.Title>{post.title.toUpperCase()}</Card.Title>
-                <Card.Text>{'${post.body.slice(0,20)}... }'}</Card.Text>
+                <Card.Text>{`${post.body.slice(0, 20)} ...}`}</Card.Text>
             </Card.Body>
+
+            <Card.Footer style ={{ textAlign: 'right', padding: 5}}>
+                <Button variant ="danger" onClick={() => deletePost(post.id)}>
+                    Delete
+                </Button>
+            </Card.Footer>
+
+
         </Card>
       ))}
     </CardColumns>
     </div>
   )
-}
+} 
